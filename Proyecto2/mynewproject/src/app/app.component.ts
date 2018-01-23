b948ab75c1e09eb92bad50367fbda2684512b82f
BABEL-SI\jesus.velasco <jesus93illes@gmail.com>
2018-01-23 16:24:11 +0100

    Prueba de modificación del componente principal

--- Proyecto2/mynewproject/src/app/app.component.ts
+++ Proyecto2/mynewproject/src/app/app.component.ts
@@ -2,9 +2,12 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
+  // template: `<h1>Bienvenido a {{title}}</h1>
+  //             <h2>{{msg}}</h2>`,
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  title = 'la clase';
+  msg = 'Este es un texto de bienvenida';
 }
