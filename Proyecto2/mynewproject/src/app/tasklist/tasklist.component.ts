0372d6dfe0f3e234b96796ddd0fd9cd4b71213a6
BABEL-SI\jesus.velasco <jesus93illes@gmail.com>
2018-01-23 17:57:30 +0100

    añadido la la dependencia

--- Proyecto2/mynewproject/src/app/tasklist/tasklist.component.ts
+++ Proyecto2/mynewproject/src/app/tasklist/tasklist.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { TaskManagerService } from '../task-manager.service';
 
 @Component({
   selector: 'app-tasklist',
@@ -7,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TasklistComponent implements OnInit {
 
-  constructor() { }
+  constructor(private service: TaskManagerService) { }
 
   ngOnInit() {
   }
