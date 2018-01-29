cdca25ff875650843e900975f9c1533dd9f7e221
BABEL-SI\jesus.velasco <jesus93illes@gmail.com>
2018-01-29 17:33:36 +0100

    Terminado ejercicio de selección de fecha y que cargue la imagen del día selecionado.

--- Proyecto2/Material/src/app/app.component.ts
+++ Proyecto2/Material/src/app/app.component.ts
@@ -9,17 +9,19 @@ import { RequestService } from './request.service';
 })
 export class AppComponent implements OnInit {
 
-  imgUrl = '';
+  urlImg = '';
   constructor(private service: RequestService) {}
 
   ngOnInit() {
   }
 
-  searchBeers() {
-    // const url = this.myDatepicker;
-    (this.service.getRequest('this.urlNext')).subscribe(
+  loadImage(name, date, other) {
+    const dateFormat = date.value.getFullYear() + '-' + date.value.getMonth() + 1 + '-' + date.value.getDate();
+    console.log(dateFormat);
+    this.urlImg = 'https://api.nasa.gov/planetary/apod?api_key=NNKOjkoul8n1CH18TWA9gwngW1s1SmjESPjNoUFo&date=' + dateFormat; /*2018-01-25*/
+    (this.service.getRequest(this.urlImg)).subscribe(
       (result) => {
-
+        this.urlImg = result.url;
         console.log(result);
       },
       (err) => console.error(err),
